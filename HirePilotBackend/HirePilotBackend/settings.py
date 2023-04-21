b6f85ee884ffe3f0830d0f9fc16403917b438b06
anne <annemachinda@gmail.com>
2023-04-21 05:40:27 +0100

    whitelisting corsheaders

--- HirePilotBackend/HirePilotBackend/settings.py
+++ HirePilotBackend/HirePilotBackend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HirePilot',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'HirePilotBackend.urls'
@@ -122,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# white listing the localhost.3000 port for React
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
