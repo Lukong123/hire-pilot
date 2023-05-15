e25416a1c2e62e70ba19c9a8a6de51c164dcf147
anne <annemachinda@gmail.com>
2023-05-15 22:16:00 +0100

    job crud: 82%

--- HirePilotBackend/HirePilot/urls.py
+++ HirePilotBackend/HirePilot/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('apinew', views.ApiOverview, name='home')
+    path('apinew', views.ApiOverview, name='home'),
+    path('create/', views.add_jobs, name='add-jobs'),
+    path('alljobs/', views.view_jobs, name='view-jobs'),
+    path('update/<int:pk>/', views.update_jobs, name='update-jobs'),
+     path('job/<int:pk>/delete/', views.delete_jobs, name='delete-jobs'),
+     
+
 ]
--- HirePilotBackend/HirePilot/views.py
+++ HirePilotBackend/HirePilot/views.py
@@ -2,6 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Jobs
 from .serializers import JobsSerializer
+from rest_framework import serializers
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 def ApiOverview(request):
@@ -14,3 +17,51 @@ def ApiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['POST'])
+def add_jobs(request):
+    job = JobsSerializer(data=request.data)
+
+    if Jobs.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+    
+    if job.is_valid():
+        job.save()
+        return Response(job.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+@api_view(['GET'])
+def view_jobs(request):
+    if request.query_params:
+        jobs = Jobs.objects.filter(**request.query_params.dict())
+    else:
+        jobs = Jobs.objects.all()
+    
+
+    if jobs:
+        serializer = JobsSerializer(jobs, many=True)
+        return Response(serializer.data)
+    
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+@api_view(['POST'])
+def update_jobs(request, pk):
+    job = Jobs.objects.get(pk)
+    data = JobsSerializer(instance=job, data=request.data)
+
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['DELETE'])
+def delete_jobs(request, pk):
+    job = get_object_or_404(Jobs, pk=pk)
+    job.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
