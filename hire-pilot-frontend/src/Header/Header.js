5941a3c0a1ac10d27674246f48812ce9777e72f6
anne <annemachinda@gmail.com>
2023-05-23 12:02:55 +0100

    solved main search bar ux problem

--- hire-pilot-frontend/src/Header/Header.js
+++ hire-pilot-frontend/src/Header/Header.js
@@ -15,6 +15,7 @@ function Header() {
   <a  href="/khome">Logo</a>
   <a href="#news">Hire Pilot</a>
   <input className='search' type='search' placeholder='Search'/> 
+
   <div class="topnav-right">
 
     <a class='active' href="/">My Jobs</a>
