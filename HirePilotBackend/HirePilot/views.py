bf76e0f103b0a5a138b544754599d65be304bf31
anne <annemachinda@gmail.com>
2023-05-28 22:27:49 +0100

    revert

--- HirePilotBackend/HirePilot/views.py
+++ HirePilotBackend/HirePilot/views.py
@@ -98,10 +98,6 @@
     
 # class NewUploadViewSet(ViewSet):
 #     serializer_class = NewUploadSerializer
-
-#     def list(self, request):
-#         return Response("GET API")
-
 #     def create(self, request):
 #         file_uploaded = request.FILES.get('file_uploaded')
 #         content_type = file_uploaded.content_type
