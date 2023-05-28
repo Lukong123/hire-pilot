47550e6455dcae9859dbc2320121c683da1f0be0
anne <annemachinda@gmail.com>
2023-05-28 21:56:16 +0100

    auth0

--- HirePilotBackend/HirePilot/models.py
+++ HirePilotBackend/HirePilot/models.py
@@ -1,80 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 # # Create your models here.
 
-# class JobSeeker(models.Model):
-# 	first_name = models.CharField(max_length=30)
-# 	last_name = models.CharField(max_length=30)
-# 	email = models.EmailField(max_length=30)
-# 	location = models.CharField(max_length=30)
-# 	phone = models.PositiveIntegerField()
-
-# 	# string representation
-# 	def __str__(self):
-
-# 		return self.first_name
-
-
-
-# class UserManager(BaseUserManager):
-# 	use_in_migration = True
-
-# 	def _create_user(self,email, name, location, password, **extra_fields):
-# 		values = [name, email, location ]
-# 		field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
-# 		for field_name, value in field_value_map.items():
-# 			if not value:
-# 				raise ValueError('The {} value must be set'.format(field_name))
-# 		email = self.normalize_email(email)
-# 		user = self.model(
-# 			email = email,
-# 			name=name,
-# 			location=location,
-# 			**extra_fields
-# 		)
-# 		user.set_password(password)
-# 		user.save(using=self._db)
-# 		return user
-    
-# 	def create_user(self, email, name, location, password=None, **extra_fields):
-# 		extra_fields.setdefault('is_staff', False)
-# 		extra_fields.setdefault('is_superuser', False)
-# 		return self._create_user(email, name, location, password, **extra_fields)
-    
-# 	def create_superuser(self, email, name, location, password=None, **extra_fields):
-# 		extra_fields.setdefault('is_staff', True)
-# 		extra_fields.setdefault('is_superuser', True)
-
-# 		if extra_fields.get('is_staff') is not True:
-# 			raise ValueError('Superuser must have is_staff=True.')
-        
-# 		if extra_fields.get('is_superuser') is not True:
-# 			raise ValueError('Superuser must have is_superser=True.')
-        
-# 		return self._create_user(email, name, location, password, **extra_fields)
-    
-
-# class User(AbstractUser, PermissionsMixin):
-# 	email = models.EmailField(unique=True)
-# 	name = models.CharField(max_length=150)
-# 	location = models.CharField(max_length=150)
-# 	picture = models.ImageField(blank=True, null=True)
-# 	is_staff = models.BooleanField(default=False)
-# 	is_active = models.BooleanField(default=True)
-# 	date_joined = models.DateTimeField(default=timezone.now)
-# 	last_login = models.DateTimeField(null=True)
-
-# 	objects = UserManager()
-# 	USERNAME_FIELD = 'email'
-# 	REQUIRED_FIELDS = ['name', 'location']
-
-# 	def get_full_name(self) -> str:
-# 		return self.name
-    
-# 	def get_short_name(self) -> str:
-# 		return self.name.split()[0]
 
 
 class UserProfileManager(BaseUserManager):
@@ -200,3 +129,17 @@ class Criteria(models.Model):
 
     
     
+# class User(AbstractUser):
+#     class Role(models.TextChoices):
+#         ADMIN = "ADMIN", 'Admin'
+#         EMPLOYER = "EMPLOYER", 'Employer'
+#         CANDIDATE = "CANDIDATE", 'Candidate'
+
+#     base_role = Role.ADMIN
+
+#     role = models.CharField(max_length=50, choices=Role.choices)
+
+#     def save(self, *arg, **kwargs):
+#         if not self.pk:
+#             self.role = self.base_role
+#             return super().save(*args, **kwargs)
