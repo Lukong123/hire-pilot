eb3ac829e26f11d8f9a44941745de4cf799d0e47
anne <annemachinda@gmail.com>
2023-06-23 14:35:16 +0100

    fetching alljobs 98%

--- hire-pilot-frontend/src/Jobs/Jobcontext.js
+++ hire-pilot-frontend/src/Jobs/Jobcontext.js
@@ -1,82 +1,41 @@
-import React, {Component} from 'react';
-import './Jobcontext.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
-const todoItems = [
-  
-  {
-    "pk": 1,
-    "company_name": "City Solution",
-    "title": "Flutter Developer",
-    "category": "Technology",
-    "location": "Bamenda",
-    "fulltime_partime": "Full Time",
-    "offline_remote": "Offline",
-    "submission_deadline": "2023-06-20",
-    "selection_step": 3,
-    "salary_range": "200000",
-    "description": "Flutter Developer needed, full time job and good working conditions",
-    "skills": "excel, social media handling, communication",
-    "date_created": "2023-06-01"
-},
 
-{
-  "pk": 1,
-  "company_name": "Fidelity Cooperative Investment",
-  "title": "Accountant",
-  "category": "Finance",
-  "location": "Bamenda",
-  "fulltime_partime": "Full Time",
-  "offline_remote": "Offline",
-  "submission_deadline": "2023-06-20",
-  "selection_step": 3,
-  "salary_range": "200000",
-  "description": "An accountant needed at the FCI branch  Bafoussam.You should have at least one year experience",
-  "skills": "excel, social media handling, communication",
-  "date_created": "2023-06-01"
-},
+function Jobcontext() {
+  const [name, setName] = useState([]);
 
-{
-  "pk": 1,
-  "company_name": "Innovation Cameroon",
-  "title": "Marketter",
-  "category": "Technology",
-  "location": "Bamenda",
-  "fulltime_partime": "Full Time",
-  "offline_remote": "Offline",
-  "submission_deadline": "2023-06-20",
-  "selection_step": 3,
-  "salary_range": "200000",
-  "description": "Digital Marketter needed to work for innovation Cameroon. Fully remote",
-  "skills": "excel, social media handling, communication",
-  "date_created": "2023-06-01"
-}
-];
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-class Jobcontext extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {todoItems};
-  } 
-  render () {
-  return (
-    <div class='jf'>
-       {this.state.todoItems.map(item => (
-        <div className='jobcontext'>
-           <div> <p> Img </p> <p class='companyname'> {item.company_name}</p> </div>
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/HirePilot/alljobs/');
+    setName(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+return (
+  <div>
+    {name.map((data) => (
+     <div className='jobcontext'>
+     <div> <p> Img </p> <p class='companyname'> {data.company_name}</p> </div>
 
 <button  class ='applybtnn' href='/apply'><a href='/apply'>Apply</a></button>
 
-<div class='jobtitle'> {item.title} <span class='dot'>.</span> <span class='location'>{item.location}</span> </div> 
+<div class='jobtitle'> {data.title} <span class='dot'>.</span> <span class='location'>{data.location}</span> </div> 
 
-<div class='jobdescription'> {item.description} </div>
+<div class='jobdescription'> {data.description} </div>
 
-<button class='partorfull'>{item.fulltime_partime}</button>
-<div class='howlong'> {item.date_created}</div>
-        </div>
-       ))}
-</div>
-  )
-}
-}
+<button class='partorfull'>{data.fulltime_partime}</button>
+<div class='howlong'> {data.date_created}</div>
+  </div>
+    ))}
+  </div>
+);
+    }
 
 export default Jobcontext;
---
+++ hire-pilot-frontend/src/Jobs/simplefetch.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import React from 'react';
+
+function Jobcontext() {
+
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    names()
+  }, []
+  )
+
+  const names = async () => {
+    const response = await fetch('http://127.0.0.1:8000/HirePilot/alljobs/');
+
+    setName(await response.json())
+  }
+  return (
+    <div>Jobcontext
+
+      {name.map((data) => {
+        return(
+          <div className='jobcontext'>
+           <div> <p> Img </p> <p class='companyname'> {data.company_name}</p> </div>
+
+<button  class ='applybtnn' href='/apply'><a href='/apply'>Apply</a></button>
+
+<div class='jobtitle'> {data.title} <span class='dot'>.</span> <span class='location'>{data.location}</span> </div> 
+
+<div class='jobdescription'> {data.description} </div>
+
+<button class='partorfull'>{data.fulltime_partime}</button>
+<div class='howlong'> {data.date_created}</div>
+        </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Jobcontext;
